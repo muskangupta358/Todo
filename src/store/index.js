9d216fdd96b50b6d4ac00abc9e36e5b651a6905b
muskangupta358 <muskan.gupta@unthinkable.co>
2022-06-06 13:14:21 +0530

    Initial setup

---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+// import { counterReducer } from "../screens/sampleScreen2";
+// import { apiReducer } from "../screens/sampleScreen";
+// import { combineReducersWrapper } from "../lib/reduxWrapper";
+
+// export default combineReducersWrapper({counterReducer,apiReducer});
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+// import rootReducer from "./index";
+// import { configureStoreWrapper } from '../lib/reduxWrapper';
+// import { loggerWrapper } from "../lib/reduxWrapper";
+
+
+// export default configureStoreWrapper({
+//     reducer : rootReducer,
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerWrapper()),
+// });
